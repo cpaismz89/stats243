d5f8b9ccdd6e56919029d67b9d182bba518f9c9e
cpaismz89 <31336477+cpaismz89@users.noreply.github.com>
2017-10-18 02:15:45 -0300

    Update FloatingFormat.R

--- ps5/FloatingFormat.R
+++ ps5/FloatingFormat.R
@@ -2,19 +2,20 @@
 FloatingFormat <- function(x){
   # Set working directory
   setwd("C:/Users/Lenovo/Desktop/UCBerkeley/3er Semestre/STATS 243/HW/HW5/")
-  
+
   # Loading libraries and functions
   library("pryr")
   source("Binary2Decimal.R")
+  source("Mantissa2Decimal.R")
   
   # Calling the bits() function
-  x <- bits(x)
-  x <- gsub(" ", "", x)
+  y <- bits(x)
+  y <- gsub(" ", "", y)
   
   # Extract Elements
-  S <- gsub(" ", "", substr(x, 1, 1))
-  e <- gsub(" ", "", substr(x, 2, 12))
-  d <- gsub(" ", "", substr(x, 13, nchar(x)))
+  S <- gsub(" ", "", substr(y, 1, 1))
+  e <- gsub(" ", "", substr(y, 2, 12))
+  d <- gsub(" ", "", substr(y, 13, nchar(y)))
   
   # Prints out the different elements of the number representation
   print(paste("Sign (S):", S, "- Length:", nchar(S), "- Number:",  
@@ -22,5 +23,10 @@ FloatingFormat <- function(x){
   print(paste("Exponent (e):", e, "- Length:", nchar(e), "- Number:",    
               Binary2Decimal(e), sep = " "))
   print(paste("Mantissa (d):", d, "- Length:", nchar(d), sep = " "))
+  print(paste("              Number: ", Mantissa2Decimal(d), sep = " "))
+  print(paste("Number representation: ", x, " = (-1)^", 
+              Binary2Decimal(S), " * (", 
+              1 + Mantissa2Decimal(d), ") * 2^(", 
+              Binary2Decimal(e), " - 1023)", 
+              sep = ""))
 }
-
